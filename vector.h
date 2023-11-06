df833139f49d17d56d69f4fb9dca73426b448754
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-11-05 22:38:24 -0800

    Add files via upload
    Vector class implementation:
    
    # TODO
    - [ ] `iterator insert()` 
    - [ ] `reserve()`
    - [ ] extra credit: `selection_sort()` algorithm

---
+++ vector.h
@@ -0,0 +1,271 @@
+// vector.h
+//
+// About:
+//*************************************************************************************************
+// A templated vector class to be used as a container for objects in the 2D modeler
+// Designed to be compatible with Qt 6.6 QPaint related objects
+//
+// Vector exists within namespace scrumptious
+//
+// Vector supports the following basic operations: 
+// constructors for one or more arguments, default 
+// constructors, copy constructor, copy assignment, move constructor, 
+// move assignment and destructor. Vector also supports a basic iterator 
+// member type and member function begin() and end() operations
+//
+// Extra Credit [+2 Pts]: Write a custom selection_sort algorithm and two 
+// custom comparison functions compare_shape_perimeter, 
+// compare_shape_area. All functions are templated. The comparison 
+// functions are called via a function pointer passed to selection_sort. 
+// This allows the algorithm to sort shapes by perimeter and area 
+// respectively. Selection_sort must sort a vector of shapes by id
+// (default) or alternatively via custom comparison functions.
+//
+//************************************************************************************************* 
+//
+//
+// Created By: Spencer Glenn
+// Date: 11/5/2023
+// Class: CS1C
+// Prof: John Kath
+//
+//*************************************************************************************************
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+
+namespace scrumptious{
+	template <class T>
+	class Vector{
+		// a templated vector class to serve as a container for Qt objects
+		private:
+			//*************************************************************************
+			// private data members
+			//_________________________________________________________________________
+			int size;  // current size of vector
+			T* vector; // pointer to dynamic array
+			int space; // nummber of elements plus number of free slots
+			//*************************************************************************
+		public:
+			//*************************************************************************
+			// Default Constructor
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: Creates a vector with a default size of 0 and space of 0
+			//*************************************************************************
+			Vector() : size{0}, vector{nullptr}, space{0} {} // inline MIL
+			//*************************************************************************
+			// Overloaded Constructor (alternate constructor)
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: Creates a vector of size `n` and space `n`
+			//*************************************************************************
+			explicit Vector(int n) : size{n}, vector{new T[n]}, space{n} {
+				for(int i = 0; i < size; i++){
+					vector[i] = T(0); // initialize templated elements to default value
+				}
+			}
+			//*************************************************************************
+			// Copy Constructor
+			//_________________________________________________________________________
+			// Pre: a vector object is passed in
+			// Post: Creates a deep copy of the vector object passed in
+			//*************************************************************************
+			Vector(const T &v) : size{v.size}, vector{new T[v.size]}, space{v.space} {
+				for(int i = 0; i < size; i++){
+					vector[i] = v.vector[i]; // copy each element of the vector
+				}
+			} 
+			//*************************************************************************
+			// Copy Assignment operator
+			//_________________________________________________________________________
+			// Pre: a vector object is passed in
+			// Post: creates a new object based on the vector object passed in
+			//*************************************************************************
+			Vector& operator=(const T &v){
+				
+				T *temp = new T[v.size]; // create a new vector
+				for(int i = 0; i < v.size; i++){
+					temp[i] = v.vector[i]; // copy each element of the vector
+				}
+				delete[] vector; // delete the old vector
+				vector = temp; // assign the new vector to the old vector
+				size = v.size; // assign the new size to the old size
+				space = v.space; // assign the new space to the old space
+				return *this; // return the new vector
+			}
+			//*************************************************************************
+			// Deconstructor
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: Deletes the vector object
+			//*************************************************************************
+			~Vector() {delete[] vector;} // delete the vector
+			//*************************************************************************
+			// access reference operator
+			//_________________________________________________________________________
+			// Pre: an index is passed in
+			// Post: returns a reference to the element at the index passed in
+			//*************************************************************************
+			T &operator[](int n) {return vector[n];} // access reference operator
+			//*************************************************************************
+			// const access reference operator
+			//_________________________________________________________________________
+			// Pre: an index is passed in
+			// Post: returns a const reference to the element at the index passed in
+			//*************************************************************************
+			const T &operator[](int n) const {return vector[n];} // const access reference operator
+			//*************************************************************************
+			// size function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns the size of the vector
+			//*************************************************************************
+			int size() const {return size;} // size function
+			//*************************************************************************
+			// capacity function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns the capacity of the vector
+			//*************************************************************************
+			int capacity() const {return space;} // capacity function
+			//*************************************************************************
+			// resize function
+			//_________________________________________________________________________
+			// Pre: a new size is passed in
+			// Post: resizes (grows) the vector to the new size passed in
+			//*************************************************************************
+			void resize(int newSize){
+				
+				reserve(newSize); // reserve space for the new size
+
+				for(int i = size; i < newSize; i++){
+					vector[i] = T(0); // initialize the new elements to default value
+				}
+				size = newSize; // assign the new size to the old size
+			}
+			//*************************************************************************
+			// push_back function
+			//_________________________________________________________________________
+			// Pre: a new element is passed in
+			// Post: adds the new element to the end of the vector; increases size by 1
+			//*************************************************************************
+			void push_back(T newElement){
+				// increases vector size by one; initialize the new element with `newElement`
+				if(space == 0){
+					reserve(8); // reserve space for 8 elements
+				}
+				else if(size == space){
+					reserve(2 * space); // reserve space for 2 * space elements
+				}
+				vector[size] = newElement; // initialize the new element with `newElement`
+				size++; // increase the size by 1
+			}
+			//*************************************************************************
+			// reserve function
+			//_________________________________________________________________________
+			// Pre: a new allocation is passed in
+			// Post: reserves space for the new allocation passed in
+			//*************************************************************************
+			void reserve(int newAlloc){
+				// never decrease allocation
+				// allocate new space
+
+				
+				// copy old elements
+				// deallocate old space
+			}
+			//*************************************************************************
+			// Iterator functions
+			//*************************************************************************
+			using iterator = T*;
+			using const_iterator = const T*;
+			//*************************************************************************
+			// begin function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns an iterator to the first element of the vector
+			//*************************************************************************
+			iterator begin() {
+				if(size == 0){
+					return nullptr;
+				}	
+				return &vector[0]; // return an iterator to the first element
+			}
+			//*************************************************************************
+			// const begin function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns a const iterator to the first element of the vector 
+			//*************************************************************************
+			const_iterator begin() const {
+				if(size == 0){
+					return nullptr;
+				}	
+				return &vector[0]; // return a const iterator to the first element
+			}
+			//*************************************************************************
+			// end function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns an iterator to one beyond the last element of the vector
+			//*************************************************************************
+			iterator end() {
+				if(size == 0){
+					return nullptr;
+				}	
+				return &vector[size]; // return an iterator to the last element
+			}
+			//*************************************************************************
+			// const end function
+			//_________________________________________________________________________
+			// Pre: None
+			// Post: returns a const iterator to one beyond the last element of the vector
+			//*************************************************************************
+			const_iterator end() const {
+				if(size == 0){
+					return nullptr;
+				}	
+				return &vector[size]; // return a const iterator to the last element
+			}
+			//*************************************************************************
+			// insert function
+			//_________________________________________________________________________
+			// Pre: an iterator and a new element are passed in
+			// Post: inserts the new element at the iterator position
+			//*************************************************************************
+			iterator insert(iterator p, const T &v){
+				// insert a new element v before p
+				// make sure we have space
+				// the place to put the value
+				// copy element one position to the right
+				// insert the new value at position p
+				// return a pointer to the newly inserted element
+				return nullptr; // temporary, remove and replace
+			}
+			//*************************************************************************
+			// erase function
+			//_________________________________________________________________________
+			// Pre: an iterator is passed in
+			// Post: erases the element at the iterator position
+			//*************************************************************************
+			iterator erase(iterator p){
+				// remove element pointed to by p
+				// copy element one position to the left
+				// return a pointer to the element pointed to by p
+				if(p == end()){
+					return p;
+				}
+				for(iterator pos = p + 1; pos != end(); ++pos){
+					*(pos - 1) = *pos; // copy element one position to the left
+				}
+				delete (end() - 1); // delete the last element
+				--size; // decrease the size by 1
+				return p; // return a pointer to the element pointed to by p
+			}
+			//*************************************************************************
+	};
+}
+
+#endif // VECTOR_H
