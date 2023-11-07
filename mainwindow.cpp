658ab635feb5be2c289af46f860f423261bd64eb
armanmha <124537341+armanmha@users.noreply.github.com>
2023-11-07 14:04:15 -0800

    committing login page to login_widget branch

---
+++ mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_LOGINBUTTON_clicked()
+{
+    QString UserName = ui->userEDIT->text();
+    QString Password = ui->passEDIT->text();
+
+    if (UserName == "Scrumptious" && Password == "CS1C")
+    {
+        QMessageBox::information(this, "ScrumptiousCS1C", "Login Successful!");
+    }
+    else
+    {
+        QMessageBox::warning(this, "ScrumptiousCS1C", "Enter username or password... / Incorrect entry");
+    }
+}
+
+
+void MainWindow::on_CANCELBUTTON_clicked()
+{
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(this, "ScrumptiousCS1C", "Are you sure you would like to close the application?", QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+    {
+        QApplication::quit();
+    }
+}
+
