dac7c5c3a65162c535c0c1d5796ac41fb741c573
EthanCortez <ethangcortez@gmail.com>
2023-11-16 15:34:07 -0800

    adding header file for the parser

---
+++ parser.h
@@ -0,0 +1,20 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+enum ShapeNames
+{
+    LINE      = 1,
+    POLYLINE  = 2,
+    POLYGON   = 3,
+    RECTANGLE = 4,
+    SQUARE    = 5,
+    ELLIPSE   = 6,
+    CIRCLE    = 7,
+    TEXT      = 8,
+};
+
+extern const QMap<ShapeNames, std::string> INPUT_SHAPE_NAMES;
+
+scrumptious::Vector<Shape*> LoadFile();
+
+#endif // PARSER_H
