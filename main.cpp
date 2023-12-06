49fc7170c2ba63713e39611591afc79449711f2c
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-12-05 21:29:05 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,13 +15,13 @@ int main(){
 
 	// Test Contents of vector
 	std::cout << "Vector v contents\n";
-	std::cout << v;
+	std::cout << *v;
 
 	// Test copy constructor 
 
 	std::unique_ptr<scrumptious::Vector<int>> v2 = std::make_unique<scrumptious::Vector<int>>(*v); 
 
-	std::cout << "Testing copy constructor\n" << v2 << "\n";
+	std::cout << "Testing copy constructor\n" << *v2 << "\n";
 	
 	// Test assignment operator
 	std::unique_ptr<scrumptious::Vector<int>> v3 = std::make_unique<scrumptious::Vector<int>>();
@@ -29,7 +29,7 @@ int main(){
 	*v3 = *v2;
 
 	// Test contents of v3
-	std::cout << "Testing assignment operator\n" << v3 << "\n";
+	std::cout << "Testing assignment operator\n" << *v3 << "\n";
 
 	// Test size
 	std::cout << "Testing size\n" << v->sizeOf() << "\n";
@@ -38,7 +38,7 @@ int main(){
 	std::unique_ptr<scrumptious::Vector<int>> v4 = std::make_unique<scrumptious::Vector<int>>(std::move(*v3));
 
 	// Test contents of v4
-	std::cout << "Testing move constructor\n" << v4 << "\n";
+	std::cout << "Testing move constructor\n" << *v4 << "\n";
 	
 	// Test size
 	std::cout << "Testing size\n" << v4->sizeOf() << "\n";
@@ -48,19 +48,19 @@ int main(){
 	// Test insert 
 	v->insert(v->begin(), 10);
 
-	std::cout << "Testing insert\n" << v << "\n";
+	std::cout << "Testing insert\n" << *v << "\n";
 	
 	// test insert
 	v->insert(v->end(), 13);
 
-	std::cout << "Testing insert\n" << v << "\n";
+	std::cout << "Testing insert\n" << *v << "\n";
 
 	// test reserve/resize 
 	v->resize(20);
 
 	std::cout << "Testing resize\n" << v->sizeOf() << "\n";
 
-	std::cout << v << "\n";
+	std::cout << *v << "\n";
 
 	// test get_iterator 
 	std::cout << "Testing get_iterator\n" << v->get_iterator(2) << "\n";
@@ -68,18 +68,18 @@ int main(){
 	// test erase
 	std::cout << "Erasing memory address: " << v->erase(v->get_iterator(2)) << "\n";
 
-	std::cout << "Testing erase\n" << v << "\n";
+	std::cout << "Testing erase\n" << *v << "\n";
 	
 	// test insert before
 	v->insert(v->get_iterator(2), 15);
 
-	std::cout << "Testing insert before\n" << v << "\n";
+	std::cout << "Testing insert before\n" << *v << "\n";
 
 	// test insert after
 
 	v->insert(v->get_iterator(3), 16);
 
-	std::cout << "Testing insert after\n" << v << "\n";
+	std::cout << "Testing insert after\n" << *v << "\n";
 
 	return 0;
 }
