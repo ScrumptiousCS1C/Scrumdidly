7053dbea9d874b36ac0b41a56c363688801ade8d
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-12-07 18:06:52 -0800

    Add files via upload
    # Latest version of UI implementation
    Needs: 
    - [ ] URL to Github repo
    - [ ] Parser Implementation
    - [ ]Serializer Implementation
    - [ ] Contact window
    - [ ] Agile portfolio
    - [ ] Zip containing Qt project
    - [ ] Zip containing only class files written specifically to proj reqs.
    - [ ] Text file documenting source files
    - [ ] Zip containing Doxygen class Documentation
    - [X] Valgrind memory leak check and Vector driver

---
+++ Gryuuum Proj/Line.h
@@ -0,0 +1,38 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include "Shape.h"
+
+class Line : public Shape {
+private:
+    QLineF line;  // Private variable specific to Line
+
+public:
+    //**********************************************************************
+    // Constructors
+    //______________________________________________________________________
+    // Default constructor initializes a sample line
+    // Another constructor allows creating a line with different dimensions
+    //**********************************************************************
+    Line(QPoint p1, QPoint p2);
+    Line();
+
+    //**********************************************************************
+    // draw()
+    //______________________________________________________________________
+    // Pre: Object exists
+    // Post: Draws the line using the specified QPainter and dimensions
+    //**********************************************************************
+    void draw(QPainter *externalPainter) override;
+
+    //**********************************************************************
+    // move(const QPoint& translationVector)
+    //______________________________________________________________________
+    // Pre: Object exists, translationVector is a valid QPoint
+    // Post: Moves the line by the specified translationVector
+    //**********************************************************************
+    void move(const QPoint& translationVector) override;
+};
+
+#endif // LINE_H
+
---
+++ Gryuuum Proj/renderscreen.h
@@ -0,0 +1,35 @@
+#ifndef MAINSCREEN_H
+#define MAINSCREEN_H
+
+#include <QMainWindow>
+#include "vector.h"
+#include "Shape.h"
+#include "Polygon.h"
+#include "Polyline.h"
+#include "Text.h"
+#include "Rectangle.h"
+#include "Ellipse.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QSpinBox>
+
+namespace Ui {
+class RenderScreen;
+}
+
+class MainScreen : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainScreen(QWidget *parent = nullptr);
+    void fillComboBox();
+    void checkSelection(QString shapeDropDownSel);
+    ~MainScreen();
+
+private:
+    Ui::RenderScreen *ui;
+    scrumptious::Vector<Shape> *shapeList;
+};
+
+#endif // MAINSCREEN_H
