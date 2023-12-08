e5a7cf174c0957f4d8ed84376b8f06657430e5bd
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-12-07 23:46:21 -0800

    Add files via upload
    Latest implementation of serializer, ui, vector, shape hierarchy, login, contact, and shape editor

---
+++ parser.h
@@ -0,0 +1,24 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <QMap>
+#include "vector.h"
+#include "Shape.h"
+
+enum ShapeNames
+{
+    LINE      = 1,
+    POLYLINE  = 2,
+    POLYGON   = 3,
+    RECTANGLE = 4,
+    SQUARE    = 5,
+    ELLIPSE   = 6,
+    CIRCLE    = 7,
+    TEXT      = 8,
+};
+
+extern const QMap<ShapeNames, std::string> INPUT_SHAPE_NAMES;
+
+scrumptious::Vector<Shape*> LoadFile();
+
+#endif // PARSER_H
---
+++ serializer.h
@@ -0,0 +1,8 @@
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+#include <QMap>
+#include "vector.h"
+#include "Shape.h"
+
+#endif
