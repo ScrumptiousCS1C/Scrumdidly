6d94c8cc1b6ee92841c1c4afe41d074bc2f7a0bc
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-12-12 10:36:42 -0800

    Add files via upload
    SHAPES NOW DRAW AND CHANGE ALL SETTINGS ALL SETTINGS ARE REFLECTED ACCURATELY

---
+++ Gryuuum 9/Ellispe.cpp
@@ -0,0 +1,21 @@
+//#include "Ellipse.h"
+
+
+//// Constructor
+//Ellipse::Ellipse()
+
+//Ellipse::Ellipse(QPoint p1, QPoint p2)
+
+//Ellipse::Ellipse(QPoint p1, QPoint p2, int id, QPen pen, QBrush brush)
+
+
+//// Implement pure virtual function from base class
+//void Ellipse::draw(QPainter *externalPainter)
+//// Implement pure virtual function from base class
+//void Ellipse::move(const QPoint& translationVector)
+//// Example: Getter for the ellipse dimensions
+//QRect Ellipse::getEllipseRect() const
+
+//// Example: Setter for the ellipse dimensions
+//void Ellipse::setEllipseRect(const QRect& newRect)
+
