2925f471234a7634ed9d79c4c214d12d5b0bbe8f
Spencer Glenn <122257729+Gryuuum@users.noreply.github.com>
2023-12-12 02:24:39 -0800

    Add files via upload
    Latest version of project major changes:
    huge rework of class hierarchy, shape and subclasses are now templated
    parser and all associated files now reflect this change
    Shape now has a templated vector of shape coordinates, this vector can be changed to accommodate any coordinate data (int/QPoint/etc)
    All classes now have a constructor dedicated for the parser

---
+++ Gryuuum 7/Polygon.h
@@ -0,0 +1,105 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "Shape.h"
+#include <QPolygon>
+
+template<typename T>
+class Polygon : public Shape<T> {
+private:
+    // Private variable specific to Polygon
+    QPolygon polygon;
+
+public:
+    //**********************************************************************
+    // Constructors
+    //______________________________________________________________________
+    // Default constructor initializes a sample polygon
+    // Other constructors allow creating polygons with different dimensions
+    //**********************************************************************
+    Polygon()
+    {
+        // Add sample points to the polygon
+        polygon << QPoint(300, 200)
+                << QPoint(400, 150)
+                << QPoint(500, 200)
+                << QPoint(475, 300)
+                << QPoint(375, 300);
+    };
+    // Constructor taking Vector<QPoint>
+    Polygon(const scrumptious::Vector<QPoint>& pointsVector)
+        : Shape<T>(3, Shape<T>::ShapeType::polygon)
+    {
+        for (int i = 0; i < pointsVector.sizeOf(); ++i) {
+            polygon << pointsVector[i];
+        }
+    }
+
+
+    Polygon(const QList<QPoint>& points, int id, QPen pen, QBrush brush)
+        : Shape<T>(id, Shape<T>::ShapeType::polygon, pen, brush)
+    {
+        polygon = QPolygon(points);
+    }
+
+    // parser constructor
+    Polygon(int id, typename Shape<T>::ShapeType type, scrumptious::Vector<T> dimensions, QPen pen)
+        : Shape<T>(id, type, dimensions, pen)
+    {
+        polygon = QPolygon(dimensions[0], dimensions[1],dimensions[2],dimensions[3],dimensions[4]);
+    }
+
+    //**********************************************************************
+    // draw()
+    //______________________________________________________________________
+    // Pre: Object exists
+    // Post: Draws the polygon using the specified QPainter and dimensions
+    //**********************************************************************
+    void draw(QPainter *externalPainter) override
+    {
+
+        if (externalPainter) {
+            this->painter = externalPainter;
+        }
+        this->painter->setPen(this->pen);
+        this->painter->setBrush(this->pen.brush());
+        this->painter->drawPolygon(polygon);
+    };
+
+    //**********************************************************************
+    // move(const QPoint& translationVector)
+    //______________________________________________________________________
+    // Pre: Object exists, translationVector is a valid QPoint
+    // Post: Moves the polygon by the specified translationVector
+    //**********************************************************************
+    void move(const QPoint& translationVector) override
+    {
+        polygon.translate(translationVector.x(), translationVector.y());
+    };
+
+    //**********************************************************************
+    // getPolygon() const
+    //______________________________________________________________________
+    // Pre: Object exists
+    // Post: Returns the QPolygon representing the polygon
+    //**********************************************************************
+    QPolygon getPolygon() const
+    {
+        return polygon;
+    };
+
+    //**********************************************************************
+    // setPolygon(const QPolygon& newPolygon)
+    //______________________________________________________________________
+    // Pre: Object exists, newPolygon is a valid QPolygon
+    // Post: Sets the polygon to newPolygon
+    //**********************************************************************
+    void setPolygon(const QPolygon& newPolygon)
+    {
+        polygon = newPolygon;
+    }
+};
+
+#endif // POLYGON_H
+
+
---
+++ Gryuuum 7/Text.h
@@ -0,0 +1,106 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include "Shape.h" // Include the base class header
+
+#include <QRect>
+#include <QString>
+#include <QColor>
+#include <QFont>
+#include <QPainter>
+
+template <typename T>
+class Text : public Shape<T> {
+public:
+    //**********************************************************************
+    // Default Constructor
+    //______________________________________________________________________
+    // Pre: none
+    // Post: Text object is initialized in heap with default values
+    //**********************************************************************
+    Text() : Shape<T>(6, Shape<T>::ShapeType::text) {}
+
+    // Parser Constructor
+    Text(int i, typename Shape<T>::ShapeType type, scrumptious::Vector<T> dimensions, QPen pen)
+        : Shape<T>(i, type, dimensions, pen)
+    {
+
+    }
+
+    //**********************************************************************
+    // Destructor
+    //______________________________________________________________________
+    // Pre: Text object must exist
+    // Post: Text object is deleted
+    //**********************************************************************
+    ~Text() override {}
+
+    //**********************************************************************
+    // setRect(const QRect& newRect)
+    //______________________________________________________________________
+    // Pre: Text object exists, newRect is passed in
+    // Post: Sets the rectangle for the text to newRect
+    //**********************************************************************
+    void setRect(const QRect& newRect) {
+        rect = newRect;
+    }
+
+
+    //**********************************************************************
+    // draw() override
+    //______________________________________________________________________
+    // Pre: Text object exists
+    // Post: Draws the text using QPainter based on the text properties
+    //**********************************************************************
+    void draw (QPainter *externalPainter)  override{
+
+        if (externalPainter) {
+            this->painter = externalPainter;
+        }
+
+        this->painter->setPen(this->pen);
+        this->painter->setFont(font);
+
+        // Draw the text
+        this->painter->drawText(rect, align, text);
+    }
+
+    //**********************************************************************
+    // move(const QPoint& translationVector) override
+    //______________________________________________________________________
+    // Pre: Text object exists, translationVector is passed in
+    // Post: Moves the text by translating its position
+    //**********************************************************************
+    void move(const QPoint& translationVector) override {
+        rect.translate(translationVector);
+    }
+
+    //**********************************************************************
+    // overrideText(const QString& newText)
+    //______________________________________________________________________
+    // Pre: Text object exists, newText is passed in
+    // Post: Overrides the current text with the new text
+    //**********************************************************************
+    void overrideText(const QString& newText) {
+        text = newText;
+    }
+
+
+    void changeAlignment(Qt::AlignmentFlag newalign){
+        align = newalign;
+    }
+
+
+    void setFont ( QFont newfont) {
+        font = newfont;
+    }
+
+
+private:
+    QRect rect;
+    QFont font;
+    QString text{"Class Project 2 - 2D Graphics Modeler"};
+    Qt::AlignmentFlag align{Qt::AlignCenter};
+};
+
+#endif // TEXT_H
